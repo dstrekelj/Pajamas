7be7d870180cc8cf2f619737d391425e611742e7
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-08-24 21:59:35 +0200

    Fix WAV header data

--- app/src/main/java/io/github/dstrekelj/toolkit/audio/PcmToWav.java
+++ app/src/main/java/io/github/dstrekelj/toolkit/audio/PcmToWav.java
@@ -25,27 +25,39 @@ public class PcmToWav {
         try {
             wav = new ByteArrayOutputStream();
             // "RIFF" header
-            wav.write(chunkId);                         // 4 B  Chunk ID
-            wav.write((int) chunkSize);                 // 4 B  Chunk size
-            wav.write(format);                          // 4 B  Format
+            wav.write(chunkId);                             // 4 B  Chunk ID
+            writeInt(wav, (int) chunkSize);                 // 4 B  Chunk size
+            wav.write(format);                              // 4 B  Format
             // "fmt " sub-chunk
-            wav.write(subChunk1Id);                     // 4 B  Sub-chunk 1 ID
-            wav.write(subChunk1Size);                   // 4 B  Sub-chunk 1 size
-            wav.write(audioFormat);                     // 2 B  Audio format
-            wav.write(numChannels);                     // 2 B  Number of channels
-            wav.write(sampleRate);                      // 4 B  Sample rate
-            wav.write(sampleRate * bytesPerChannel);    // 4 B  Byte rate
-            wav.write(bytesPerChannel);                 // 2 B  Block align
-            wav.write(bitsPerSample);                   // 2 B  Bits per sample
+            wav.write(subChunk1Id);                         // 4 B  Sub-chunk 1 ID
+            wav.write(subChunk1Size);                       // 4 B  Sub-chunk 1 size
+            wav.write(audioFormat);                         // 2 B  Audio format
+            wav.write(numChannels);                         // 2 B  Number of channels
+            writeInt(wav, sampleRate);                      // 4 B  Sample rate
+            writeInt(wav, sampleRate * bytesPerChannel);    // 4 B  Byte rate
+            writeShort(wav, (short)bytesPerChannel);               // 2 B  Block align
+            writeShort(wav, bitsPerSample);                 // 2 B  Bits per sample
             // "data" sub-chunk
-            wav.write(subChunk2Id);                     // 4 B  Sub-chunk 2 ID
-            wav.write(subChunk2Size);                   // 4 B  Sub-chunk 2 size
-            wav.write(pcmData);                         // * B  Sound data
+            wav.write(subChunk2Id);                         // 4 B  Sub-chunk 2 ID
+            writeInt(wav, subChunk2Size);                   // 4 B  Sub-chunk 2 size
+            wav.write(pcmData);                             // * B  Sound data
             wav.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
         return wav.toByteArray();
+    }
+
+    private static void writeInt(ByteArrayOutputStream out, int value) throws IOException {
+        out.write(value);
+        out.write(value >> 8);
+        out.write(value >> 16);
+        out.write(value >> 24);
+    }
+
+    private static void writeShort(ByteArrayOutputStream out, short value) throws IOException {
+        out.write(value);
+        out.write(value >> 8);
     }
 }
